d1fa8458e83711c162deea68e4c282c05f4bd32f
BnXbots <129577122+BnXbots@users.noreply.github.com>
2023-05-15 17:30:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ async def start(event):
 
 @sree.on(events.NewMessage(pattern="^/help"))
 async def start(event):
-    buttns = [Button.url("••𝗦𝗨𝗣𝗣𝗢𝗥𝗧••", "https://t.me/bonten_mainchats"), Button.url("••𝗖𝗛𝗔𝗡𝗡𝗘𝗟••", "https://t.me/Bonten_Destroyers")]     py = platform.python_version()
+    buttns = [Button.url("••𝗦𝗨𝗣𝗣𝗢𝗥𝗧••", "https://t.me/bonten_mainchats"), Button.url("••𝗖𝗛𝗔𝗡𝗡𝗘𝗟••", "https://t.me/Bonten_Destroyers")]   
+    py = platform.python_version()  
     if event.sender.id in OP:
         await sree.send_file(
             event.chat.id,
